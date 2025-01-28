283938004c33e0ed629fda370da10abf3cd3e0b5
Guilherme Castelão <guilherme@castelao.net>
2025-01-28 15:07:39 -0700

    Initializing (#1)
    * cfg: Initializing Pixi definition
    
    * cfg: Git ignoring and attributes defintions
    
    * [wip] mylib
    
    * [wip] Main development script
    
    * fix: Inside function - per DataArray
    
    * Back to zarr version 2 for now
    
    * fix: zarr_version is deprecated
    
    * cfg: Creating a pixi dev environment
    
    * cfg: Updating pixi.lock
    
    * cfg: Ignoring __pycache__
    
    * [wip] Formalizing benchmarks
    
    * feat: Function to fix time
    
    - dimension time instead of phony
    - time with a useful dtype
    
    * Defining compressor with new syntax
    
    * Using custom function fix_time
    
    * Using zarr_format 3
    
    * Increasing chunk size
    
    * fix: compressor is deprecated, use compressors instead
    
    * Defining some attributes
    
    * cfg: Updating pixi definition
    
    * Prototype of benchmark
    
    * fix: pixi.toml

---
+++ src/convert_wtk.py
@@ -0,0 +1,111 @@
+import dask
+from dask.distributed import LocalCluster, Client
+import h5py
+import netCDF4
+from numcodecs import Blosc
+import pandas as pd
+import xarray as xr
+import zarr.codecs
+
+from mylib import *
+
+
+def dev(h5filename, output_path, demo=False):
+    """Big mess. Code in development"""
+
+    ds = xr.open_mfdataset(h5filename, mask_and_scale=False, engine="netcdf4")
+    ds = fix_time(ds)
+
+    ds["lat"] = ds.coordinates.isel(phony_dim_1=0)
+    ds["lon"] = ds.coordinates.isel(phony_dim_1=1)
+    ds = ds.drop_vars(["coordinates"])
+    ds = ds.rename_dims({"phony_dim_0": "location"})
+    ds = ds.set_coords(["lat", "lon"])
+
+    for v in extract_meta(h5filename):
+        ds[v.name] = v
+
+    for v in ds:
+        ds[v] = fix_variable(ds[v])
+
+    ds.temperature_10m.attrs["standard_name"] = "air_temperature"
+    ds.windspeed_10m.attrs["standard_name"] = "wind_speed"
+    ds.winddirection_10m.attrs["standard_name"] = "wind_to_direction"
+
+    encoding_per_type = {
+        "inversemoninobukhovlength": {
+            "dtype": "i2",
+            "scale_factor": 0.01,
+        },
+        "precipitationrate": {
+            "dtype": "u2",
+            "scale_factor": 0.1,
+        },
+        "pressure": {
+            "dtype": "u2",
+            "scale_factor": 0.1,
+        },
+        "relativehumidity": {
+            "dtype": "u2",
+            "scale_factor": 0.01,
+        },
+        "temperature": {
+            "dtype": "i2",
+            "scale_factor": 0.01,
+            # "add_offset": -100,
+        },
+        "windspeed": {
+            "dtype": "u2",
+            "scale_factor": 0.01,
+        },
+        "winddirection": {
+            "dtype": "u2",
+            "scale_factor": 0.01,
+        },
+    }
+
+    varnames = [v for v in ds if v.split("_")[0] in encoding_per_type]
+    encoding = {}
+
+    # from numcodecs import Blosc
+    # compressor = Blosc(cname="zstd", clevel=9, shuffle=Blosc.SHUFFLE)
+    compressors = zarr.codecs.BloscCodec(cname="zstd", clevel=9, shuffle=zarr.codecs.BloscShuffle.shuffle)
+
+    for v in varnames:
+        encoding[v] = encoding_per_type[v.split("_")[0]]
+        encoding[v]["_FillValue"] = netCDF4.default_fillvals[encoding[v]["dtype"]]
+        encoding[v]["compressors"] = compressors
+
+    # subset for development
+    # ds = ds[[v for v in ds if v.startswith("temperature") or v.startswith("pressure")]]
+    # ds = ds.isel(point=range(0, ds.point.size, 100_000))
+    if demo:
+        ds = ds.isel(location=range(100_000))
+    # 1_000 x 1_000 ~= 1MB
+    # ds.isel(point=range(0, ds.point.size, 1_000)
+    # ds[varnames].isel(point=range(100_000))
+    ds.chunk({"time": -1, "location": 3_000}).to_zarr(
+        output_path,
+        zarr_format=3,
+        encoding=encoding,
+    )
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+    cluster = LocalCluster(
+        n_workers=2,
+        memory_limit=0.15,
+        processes=True,
+        threads_per_worker=4,
+        # security=True,
+    )
+    ## client = Client(cluster.scheduler.address)
+    client = cluster.get_client()
+
+    h5filename = "/datasets/WIND/conus/v1.0.0/wtk_conus_2013.h5"
+
+    zarr_path = Path("/scratch/gpimenta/zarr")
+
+    dev(h5filename, output_path=zarr_path / "WTK_dev.zarr", demo=True)
---
+++ src/mylib.py
@@ -0,0 +1,116 @@
+import h5py
+import pandas as pd
+import xarray as xr
+
+
+def extract_meta(filename):
+    """Extract variables embeded in 'meta'"""
+    with h5py.File(filename, "r") as h5f:
+        for vname in h5f["meta"].dtype.names:
+            yield xr.DataArray(
+                h5f["meta"][vname][:],
+                name=vname,
+                dims=("location",),
+                attrs=dict(
+                    description="Extracted from meta variable",
+                ),
+            )
+
+
+def fix_variable(da):
+    attrs = da.attrs
+    encoding = da.encoding
+    encoding = {}
+    if "fill_value" in da.attrs:
+        print(f"Fixing fill value for {da.name}, {da.attrs['fill_value']}")
+        da = da.where(da != da.attrs["fill_value"])
+        attrs.pop("fill_value")
+        # encoding["_FillValue"] = da.attrs.pop("fill_value")
+
+    if "scale_factor" in da.attrs:
+        if "adder" in da.attrs:
+            da.attrs["offset"] = da.attrs["adder"]
+            da = da + da.attrs["offset"]
+
+        else:
+            print(f"Fixing scaling factor for {da.name}, {da.attrs['scale_factor']}")
+            da = da / da.attrs["scale_factor"]
+            # da.encoding["scale_factor"] = 1 / attrs.pop("scale_factor")
+
+        attrs.pop("scale_factor")
+
+    da.attrs = attrs
+    # da.encoding = encoding
+    return da
+
+def fix_time(ds):
+    """Fix dimension name and data type for time
+
+    Using phony_dim_X is not informative. Instead let's call it time.
+
+    The actual time is stored as a string. Although that is easy for a
+    human to read, it is not actionable. Any operation with time would
+    require first validate if that string is a valid date/time, then
+    convert that to some actionable data type, such as np.datetime64.
+    Another issue is the space used. This string takes 19B versus the
+    8B used by np.datetime64 which has ns resolution.
+    """
+    assert "time_index" in ds
+
+    # Rename the phony dimension to time
+    assert len(ds["time_index"].dims) == 1
+    ds = ds.rename_dims({ds["time_index"].dims[0]: "time"})
+
+    assert ds["time_index"].dtype.kind == "U", "Expected a `time_index` of type unicode"
+    ds["time"] = pd.DatetimeIndex(ds["time_index"].values)
+
+    # Figure out which type of calendar it is
+    # ds.["time"].attrs["calendar"]
+
+    # We don't need this anymore
+    ds = ds.drop_vars(["time_index"])
+
+    return ds
+
+
+standard_attributes = {
+    "time": {
+        "long_name": "time",
+        "calendar": "proleptic_gregorian",
+    }
+    "latitude": {
+        "standard_name": "latitude",
+        "units": "degree_north",
+    },
+    "longitude": {
+        "standard_name": "longitude",
+        "units": "degree_east",
+    },
+    "temperature": {
+        "standard_name": "air_temperature",
+        "units": "C",
+    },
+    "windspeed": {
+        "standard_name": "wind_speed",
+        "units": "m s-1",
+    },
+    "winddirection": {
+        "standard_name": "wind_to_direction",
+        "units": "degree",
+    },
+    "pressure": {
+        "standard_name": "air_pressure",
+        "units": "Pa",
+    },
+    "relativehumidity": {
+        "standard_name": "relative_humidity",
+        "units": 1,
+    },
+}
+
+def add_attributes(ds):
+    for v in ds:
+        if attrs:=v.split("_")[0] in standard_attributes:
+            for k, v in standard_attributes[attrs].items():
+                ds[v].attrs[k] = v
+
